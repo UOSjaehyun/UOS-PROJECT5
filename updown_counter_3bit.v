c8d184f55aa367607f1d04ce5a81b72c4bd851d1
UOSjaehyun <jmy010311@uos.ac.kr>
2025-09-29 03:23:45 +0900

    Add files via upload

--- updown_counter_3bit.v
+++ updown_counter_3bit.v
@@ -1,35 +1,27 @@
 `timescale 1ns / 1ps
 
-module updown_counter_3bit (
-    input wire clk, reset,
-    output reg [2:0] q
+module up_counter_oneshot (
+    input wire clk,
+    input wire reset,
+    input wire input_sm1,
+    output reg [2:0] state
 );
 
-    reg direction_is_up; 
+    reg input_sm1_reg;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            q <= 3'b000;
-            direction_is_up <= 1'b1; 
+            state <= 3'b000;
+            input_sm1_reg <= 1'b0;
         end
         else begin
-            if (direction_is_up) begin 
-                if (q == 3'b111) begin
-                    direction_is_up <= 1'b0; 
-                    q <= q - 1'b1;           
-                end
-                else begin
-                    q <= q + 1'b1;           
-                end
-            end
-            else begin
-                if (q == 3'b000) begin 
-                    direction_is_up <= 1'b1;
-                    q <= q + 1'b1;           
-                end
-                else begin
-                    q <= q - 1'b1;           
-                end
+            input_sm1_reg <= input_sm1;
+
+            if (input_sm1 && !input_sm1_reg) begin
+                if (state == 3'b111)
+                    state <= 3'b000;
+                else
+                    state <= state + 1'b1;
             end
         end
     end
